588e19c79f64839fb9824afe95b038e586d666b4
tal <tal63566@gmail.com>
2019-12-30 16:40:03 +0200

    test

---
+++ public/precache-manifest.a4ddc65f096de618df4900dfd2e5875a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c0e473f8bf0828f26211e73fbb1a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "890eb6d6266475d1d618",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9de60b7954a479f1f4e7",
+    "url": "/static/js/2.3f64b8a3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3f64b8a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "890eb6d6266475d1d618",
+    "url": "/static/js/main.7f7f56d1.chunk.js"
+  },
+  {
+    "revision": "dbdce0b7c9ca263206a7",
+    "url": "/static/js/runtime-main.be5a3386.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
